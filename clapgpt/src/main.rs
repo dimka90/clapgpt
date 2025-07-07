3445283ac75346f548ca818d7b3410bfb9e8a444
dimka90 <dimkayilrit@gmail.com>
2025-07-07 07:38:52 +0100

    feat: parse user command from the terminal and parsing to ai for processing

--- clapgpt/src/main.rs
+++ clapgpt/src/main.rs
@@ -1,10 +1,16 @@
-
+use termimad::MadSkin;
 use std::process;
-
+use std::env;
 use clap::{Command, Arg};
-
-fn main() {
-
+use dotenvy::dotenv;
+use serde_json::json;
+use reqwest::Client;
+#[tokio::main]
+async  fn main() -> Result <(), Box<dyn std::error::Error>> {
+     let skin = MadSkin::default();
+    dotenv().ok();
+    let api_key = env::var("OPEN_AI_API_KEY")?;
+    //open ai model data
     let matches = Command::new("clapgpt")
     .about("An ai command-line tool")
     .version("0.1")
@@ -19,19 +25,38 @@ fn main() {
     .get_matches();
 
     match  matches.subcommand() {
-
         Some(("clapgpt", arg_matches)) => {
             if let Some(cmd_argument) = arg_matches.get_one::<String>("cmd_arg"){
 
-            println!("Command line argument {:?}", cmd_argument);
-            }
+            let body = json!({
+                "model": "gpt-4o-mini",
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": cmd_argument
+                    }
+                ],
+            });
+            // make api call to openai
+            let client = Client::new();
 
+            let res = client
+            .post("https://api.openai.com/v1/chat/completions")
+            .bearer_auth(api_key)
+            .json(&body)
+            .send()
+            .await?;
+
+            let json_response:  serde_json::Value = res.json().await?;
+            let content = json_response["choices"][0]["message"]["content"].as_str().unwrap_or("No response from open AI");
+            skin.print_text(content);
+            // println!("{:?}", content);
+            }
         },
         _ => {
-
             eprintln!("Invalid input");
             process::exit(1);
         }
     }
-    println!("Hello, world!");
+    Ok(())
 }
